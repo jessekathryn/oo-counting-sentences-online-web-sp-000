0b9b0ea20877e001b82f954df448106364b2176b
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-02 02:03:34 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,10 @@ end
 end
 
  def count_sentences
+<<<<<<< HEAD
    self.split(/\.[^.]|\?|\![^!]/).count
+=======
+   self.split(/\.|\?|\!/).count
+>>>>>>> b0545d5779fafd600237ea6ade4a5240c7a3dbf0
   end
 end
